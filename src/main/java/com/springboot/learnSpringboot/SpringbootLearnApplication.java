b9bd48538fe3aac198c28d4aef4862eab770448e
xnzhenghailong <xnzhenghailong@jd.com>
2019-09-20 14:30:23 +0800

    项目初始化

---
+++ src/main/java/com/springboot/learnSpringboot/SpringbootLearnApplication.java
@@ -0,0 +1,16 @@
+package com.springboot.learnSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhenghailong
+ */
+@SpringBootApplication
+public class SpringbootLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLearnApplication.class, args);
+    }
+
+}
